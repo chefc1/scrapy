d8522adcce601fb78f227b42e281bc4956f1c971
chefc1 <146315281+chefc1@users.noreply.github.com>
2023-10-03 23:32:41 -0500

    Add files via upload

--- mercado/spiders/spider.py
+++ mercado/spiders/spider.py
@@ -21,16 +21,15 @@ class MercadoSpider(CrawlSpider):
 	def parse_item(self, response):
 		ml_item = MercadoItem()
 		#info de producto
-		ml_item['titulo'] = response.xpath('normalize-space(//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/h1/text())').extract_first()
-		ml_item['precio'] = response.xpath('normalize-space(//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[3]/div[1]/div[1]/span[1]/span[3]/text())').extract()
-		ml_item['condicion'] = response.xpath('normalize-space(//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/span/text())').extract()
-		ml_item['envio'] = response.xpath('normalize-space(//*[@id="buybox-form"]/div[1]/div/div/p[1]/text())').extract()
-		ml_item['opiniones'] = response.xpath('normalize-space(//*[@id="reviews_capability_v3"]/div/section/div/div[1]/article/div/div[1]/div[1]/p/text())').extract()
-		ml_item['ventas_producto'] = response.xpath('normalize-space(//strong[@class="ui-pdp-seller__sales-description"]/text())').extract()
-		#info de la tienda o vendedor
-		ml_item['vendedor_url'] = response.xpath('//*[@id="ui-pdp-main-container"]/div[2]/div/div[2]/div[1]/a/@href').extract()
+		ml_item['titulo'] = response.xpath('normalize-space(//h1[@class="ui-pdp-title"])').extract()
+		ml_item['precio'] = response.xpath('normalize-space(//div[@class="ui-pdp-price__second-line"]/span[@class="andes-money-amount ui-pdp-price__part andes-money-amount--cents-superscript andes-money-amount--compact"]/span[@class="andes-money-amount__fraction"])').extract()
+		ml_item['condicion'] = response.xpath('normalize-space(//span[@class="ui-pdp-subtitle"])').extract()
+		ml_item['envio'] = response.xpath('normalize-space(//span[@class="ui-pdp-color--GREEN ui-pdp-family--SEMIBOLD"])').extract()
+		ml_item['opiniones'] = response.xpath('normalize-space(//p[@class="ui-review-capability__rating__average ui-review-capability__rating__average--desktop"])').extract()
+		ml_item['ventas_producto'] = response.xpath('normalize-space(//strong[@class="ui-pdp-seller__sales-description"])').extract()
+		ml_item['vendedor_url'] = response.xpath('//a[@class="ui-pdp-media__action ui-box-component__action"]/@href').extract()
 
 		self.item_count += 1
-		if self.item_count > 100:
+		if self.item_count > 10:
 			raise CloseSpider('item_exceeded')
 		yield ml_item
